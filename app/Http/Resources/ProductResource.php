6b91e165c02e16e06902e425bb3849cdb5591dab
Filip <bondzulicfilip@gmail.com>
2025-03-19 21:47:07 +0100

    Removed the default data wrapper from API responses

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,7 @@ class ProductResource extends JsonResource
      * @return array<string, mixed>
      */
    // app/Http/Resources/ProductResource.php
+    public static $wrap = null;
     public function toArray($request)
     {
         return [
